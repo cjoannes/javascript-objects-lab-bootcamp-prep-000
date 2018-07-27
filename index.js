5c7e9fbc2e4ab1773c6c026fe7cd1c4ec6642272
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-27 17:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = new Object({ recipe: 'cake' })
 function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({}, object, { [key]: value }) }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { var Object = prop2: 2; return Object }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { var Object = {prop2: 2 } ; return Object }
 function deleteFromObjectByKey(object, key) { return delete Object.key }
 function destructivelyDeleteFromObjectByKey(object, key) { delete Object.key; return Object }
